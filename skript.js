c86d79c8d38505dc67ae456c514c5036686d3839
Maksim Smirnov <perforator01@gmail.com>
2022-05-27 16:49:21 +0300

    второй урок готов

--- skript.js
+++ skript.js
@@ -1,12 +1,16 @@
 'use strict';
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'js course';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 736;
+let rollback = 45;
+let fullPrice = 12000;
+let adaptive = false;
 
-alert('Hello world!');
-
-console.log('Hello');
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice + '₽');
+console.log(fullPrice + '₽');
+screens.toLowerCase();
+console.log(screens.split(', '));
+console.log(fullPrice * (rollback / 100));
